9acf96e127c22f791a2c78dda873ab299e7dd474
MontaKr <zhoon95@gmail.com>
2026-08-30 18:30:55 +0900

    feat : Product Viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import ProductVIewer from "./components/ProductVIewer";
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductVIewer />
     </main>
   );
 };
